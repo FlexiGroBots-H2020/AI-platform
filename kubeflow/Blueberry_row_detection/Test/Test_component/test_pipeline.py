d4523754582522f18c49120a8069c54fbd807362
Dimitrije2507 <dimitrijestefanovic97@gmail.com>
2023-12-22 15:59:07 +0100

    padding/unpadding updated

--- kubeflow/Blueberry_row_detection/Test/Test_component/test_pipeline.py
+++ kubeflow/Blueberry_row_detection/Test/Test_component/test_pipeline.py
@@ -233,7 +233,9 @@ def test_block(load_data_pth,model_pth,save_pred_pth, net_type, device):
     # model.load_state_dict(torch.load(r'/home/stefanovicd/DeepSleep/GraphNeuralNetworks/Deeplab-Large-FOV-master/saved_models/GCNModel_2000segmenata_5Sobel_kanala_treningVeci_test_h128_lr2nule_k16.pt',map_location=torch.device('cpu')))
     print("Test block ended")
     
-def postprocessing_block(GeoTiff,full_geotiff,y0,y1,x0,x1,final_mask, geotransform, projection, load_pred_pth,save_final_GeoTiff_pth,save_final_colored_GeoTiff_pth,save_final_shp_pth):
+def postprocessing_block(GeoTiff,full_geotiff,y0,y1,x0,x1,final_mask,
+                         geotransform, projection, load_pred_pth,save_final_GeoTiff_pth,
+                         save_final_colored_GeoTiff_pth,save_final_shp_pth,deltas):
     print("Postprocessing block started")
     
     colors_grey = np.array([255, 200, 155, 100, 55], dtype="uint8")
@@ -267,10 +269,10 @@ def postprocessing_block(GeoTiff,full_geotiff,y0,y1,x0,x1,final_mask, geotransfo
 
                 print('Final test sample '+str(counter))
                 counter+=1
-
-    # create the 3-band raster file
     
-    np.save(os.path.join(save_final_GeoTiff_pth,"detected_rows_mask.npy"),Final_prediction*255)
+    # create the 3-band raster file (left_delta,top_delta,right_delta,bottom_delta)
+    np.save(os.path.join(save_final_GeoTiff_pth,"detected_rows_mask.npy"),Final_prediction[deltas[1]:-deltas[3],deltas[0]:-deltas[2]]*255)
+    # np.save(os.path.join(save_final_GeoTiff_pth,"detected_rows_mask.npy"),Final_prediction*255)
     final_mask[y0:y1,x0:x1] = copy.deepcopy(Final_prediction)*255
     # full_geotiff[:,:,0][final_mask//255] = 255
 
@@ -372,63 +374,7 @@ def postprocessing_block(GeoTiff,full_geotiff,y0,y1,x0,x1,final_mask, geotransfo
     RGB_raster = GeoTiff[:,:,0:3]
     RGB_raster    
     
-    
-# def postprocessing_block(GeoTiff,y0,y1,x0,x1,final_mask, geotransform, projection, load_pred_pth,save_final_GeoTiff_pth,save_final_colored_GeoTiff_pth,save_final_shp_pth):
-#     print("Postprocessing block started")
 
-#     H = np.shape(GeoTiff[:,:,0])[0]
-#     W = np.shape(GeoTiff[:,:,0])[1]
-#     patch_size = 512
-#     tmp_labels = np.zeros([patch_size,patch_size])
-#     tmp_rgb = np.zeros([patch_size,patch_size,3])
-#     # tmp_nir = np.zeros([patch_size,patch_size])
-#     # tmp_red_edge = np.zeros([patch_size,patch_size])
-#     tmp_svi_kanali_combo = np.zeros([patch_size,patch_size])
-#     Final_prediction = np.zeros_like(GeoTiff[:,:,0],dtype='uint8')
-#     testing_flag = True
-#     import re
-#     samples = os.listdir(load_pred_pth)
-#     samples = sorted(samples, key=lambda s: int(re.search(r'\d+', s).group()))
-#     counter = 0
-#     for i in range(H//patch_size):
-
-#         for j in range(W//patch_size):
-
-#             tmp_labels = GeoTiff[i*patch_size:i*patch_size+patch_size,j*patch_size:j*patch_size+patch_size,5]
-
-
-#             png_flag = False
-#             if np.sum(tmp_labels)!= 0:
-
-#                 Final_prediction[i*patch_size:i*patch_size+patch_size,j*patch_size:j*patch_size+patch_size] = np.load(load_pred_pth+'/'+samples[counter])
-
-#                 print('Final test sample '+str(counter))
-#                 counter+=1
-
-#     # # create the 3-band raster file
-#     # dst_ds = gdal.GetDriverByName('GTiff').Create('myGeoTIFF.tif', W, H, 1, gdal.GDT_Byte)
-
-#     # dst_ds.SetGeoTransform(geotransform)    # specify coords
-#     # srs = osr.SpatialReference()            # establish encoding
-#     # srs.ImportFromEPSG(32634)                # WGS84 lat/long
-#     # dst_ds.SetProjection(srs.ExportToWkt()) # export coords to file
-#     # dst_ds.GetRasterBand(1).WriteArray(Final_prediction)   # write r-band to the raster
-#     # # dst_ds.GetRasterBand(2).WriteArray(g_pixels)   # write g-band to the raster
-#     # # dst_ds.GetRasterBand(3).WriteArray(b_pixels)   # write b-band to the raster
-#     # dst_ds.FlushCache()                     # write to disk
-
-#     srsProj = osr.SpatialReference()
-#     srsProj.ImportFromEPSG(32634)
-#     final_mask[y0:y1,x0:x1] = copy.deepcopy(Final_prediction)
-#     # final_mask[y0 - row_pad:y_end + row_pad, x_start - column_pad:x_end + column_pad] = copy.deepcopy(image)
-#     driver = gdal.GetDriverByName('GTiff')
-#     tmp_raster = driver.Create(save_final_GeoTiff_pth + '/' + 'test_parcel.tif', final_mask.shape[1], final_mask.shape[0], 1, gdal.GDT_Byte)
-#     tmp_raster.SetGeoTransform(geotransform)
-#     tmp_raster.SetProjection(projection)
-#     srcband = tmp_raster.GetRasterBand(1)
-#     srcband.WriteArray(final_mask)
-#     srcband.FlushCache()
-#     print("Postprocessing block ended")
 
 def main(model_path, net_type, device, input_files_type=None):
     
@@ -584,7 +530,8 @@ def main(model_path, net_type, device, input_files_type=None):
     y0_new = y0 - top_delta
     x1_new = x1 + right_delta
     y1_new = y1 + bottom_delta
-
+    deltas = (left_delta,top_delta,right_delta,bottom_delta)
+    
     np.save(main_path+"/DataTest/cropped_mask.npy",cropped_mask)
     save_test_data_pth = main_path + "/DataTest/test_data_folder"
     if os.path.isdir(save_test_data_pth)==False:
@@ -610,7 +557,11 @@ def main(model_path, net_type, device, input_files_type=None):
         os.mkdir(save_final_GeoTiff_pth)
     save_final_colored_GeoTiff_pth = main_path + "/DataTest/final_results_folder/"
     save_final_shp_pth = main_path + "/DataTest/final_results_folder"
-    postprocessing_block(stacked_geotiffs_npy,full_stacked_geotiffs,y0_new,y1_new,x0_new,x1_new,final_mask, geo_transform,wkt_raster_proj,load_pred_data_pth, save_final_GeoTiff_pth, save_final_colored_GeoTiff_pth, save_final_shp_pth)
+    postprocessing_block(stacked_geotiffs_npy,full_stacked_geotiffs,
+                         y0_new,y1_new,x0_new,x1_new,final_mask,
+                         geo_transform,wkt_raster_proj,load_pred_data_pth,
+                         save_final_GeoTiff_pth, save_final_colored_GeoTiff_pth,
+                         save_final_shp_pth,deltas)
     
     return save_final_colored_GeoTiff_pth
 
